9e267f208e03ad9dbb7b59e00f5128edd3fb836e
Matthew Maggio <me@techwizmatt.info>
2022-01-10 20:03:32 -0700

    Finished

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const app = require('express')()
 const http = require('http')
 const path = require('path')
 const parser = require('body-parser')
+const controllers = require(path.join(process.cwd(), '/controllers'))
+
 
 app.use(parser.urlencoded({
     extended: true
@@ -13,4 +15,7 @@ app.use('/', require(path.join(process.cwd(), '/routes')))
 
 app.listen(3030, () => {
     console.log(`API RUNNING ON 3030`)
+    controllers.ludela.doSetEightHour().then(data => {
+        console.log('SET 8 HOUR TIMER')
+    })
 })
--- controllers/ludela.js
+++ controllers/ludela.js
@@ -1,43 +1,76 @@
 const { exec } = require('child_process')
+lirc_node = require('lirc_node')
+lirc_node.init()
 
 const ludela = {
+    timeout: null,
     state: 0,
     doSetEightHour: async function () {
-        return this._doSendCommand('BTN_8')
+        return new Promise((resolve, reject) => {
+            this._doIrSend("BTN_8").then(_ => {
+                resolve()
+            })
+        })
     },
     doSetPowerOn: async function () {
         return new Promise((resolve, reject) => {
-            this._doSendCommand('KEY_POWER').then(_ => {
-                ludela.state = 1
+            this._doIrSend("KEY_POWER").then(_ => {
                 resolve()
-            }).catch(_ => {
-              reject(new Error())
+                this.state = 1
+                this._clearTimeout()
+                this.timeout = setTimeout(() => {
+                    this.doSetPowerOff()
+                }, 28800 * 1000) // 8 Hours
             })
         })
     },
     doSetPowerOff: async function () {
         return new Promise((resolve, reject) => {
-            this._doSendCommand('KEY_PAUSE').then(_ => {
-                ludela.state = 0
+            this._doIrSend("KEY_PAUSE").then(_ => {
                 resolve()
-            }).catch(_ => {
-                reject(new Error())
+                this.state = 0
+                this._clearTimeout()
             })
         })
     },
-    _doSendCommand: function (command) {
+    _clearTimeout: function () {
+        try {
+            clearTimeout(this.timeout)
+        } catch (e) {
+            console.log('No timeout necassary')
+        }
+    },
+    _doIrSend: async function (command) {
         return new Promise((resolve, reject) => {
-            console.log(`EXECUTING ${command}`)
-            exec(`irsend SEND_ONCE ludela ${command}`, (err, stdout) => {
-                console.log(stdout)
-                if (err) {
-                    reject()
-                } else {
-                    resolve()
-                }
+            const promises = []
+
+            for (let i = 0; i < 10; i++) {
+                promises.push(new Promise((done) => {
+                    lirc_node.irsend.send_once("ludela", command, setTimeout(function () {
+                        console.log(`SENT COMMAND ${command}`)
+                        resolve()
+                    }, 1000))
+                }))
+            }
+
+            Promise.all(promises).then(_ => {
+                resolve()
             })
         })
     }
+    // _doSendCommandLine: function (command) {
+    //     return new Promise((resolve, reject) => {
+    //         console.log(`EXECUTING ${command}`)
+    //         exec(`irsend SEND_ONCE ludela ${command}`, (err, stdout) => {
+    //             console.log(stdout)
+    //             if (err) {
+    //                 reject()
+    //             } else {
+    //                 resolve()
+    //             }
+    //         })
+    //     })
+    // }
 }
 
 module.exports = ludela
